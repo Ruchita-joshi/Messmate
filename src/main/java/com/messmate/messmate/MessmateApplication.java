09a8f010a53e3491e676cbd90030b21ac950e99a
Ruchita Joshi <ruchitajoshi020@gmail.com>
2026-04-30 20:25:43 +0530

    Initial Project Setup

---
+++ src/main/java/com/messmate/messmate/MessmateApplication.java
@@ -0,0 +1,13 @@
+package com.messmate.messmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessmateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessmateApplication.class, args);
+    }
+
+}
